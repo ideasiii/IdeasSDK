bf1b28ededf30c3e8c8705a2a84b662873dc2600
ideasiii <louiesju@iii.org.tw>
2015-12-18 18:01:47 +0800

    add ad 12_18 joe

--- src/sdk/ideas/common/Common.java
+++ src/sdk/ideas/common/Common.java
@@ -5,7 +5,11 @@ import android.os.Message;
 
 public abstract class Common
 {
-	public static String Version = "0.15.12.16";
+	public final static String BANNER_AD_UNIT_ID       = "ca-app-pub-4351303625351134/2404547206";
+	public final static String INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-4351303625351134/4481552801";
+	public final static int TYPE_BANNER_AD 	    	   = 1;
+	public final static int TYPE_INTERSTITIALAD 	   = 2;
+	public static String Version = "0.15.12.18";
 	
 	public static String URL_APPSENSOR_INIT 			= "54.199.198.94";
 	public static int 	 PORT_APPSENSOR_INIT 			= 6607;
